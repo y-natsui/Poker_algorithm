7888dc65d339063c68e8109e0e7623f4403b3313
Yoshihiro Natsui <voila.um.homme@gmail.com>
2014-10-20 02:36:15 +0900

    修正
    エラーチェックやスコア計算において関数化しコードを簡素化

--- poker.c
+++ poker.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-int main(){
-  //こちらにカードの手を入力して下さい
-  int card[5] = {1,1,2,2,2};
-
-  int tmp;
-  int i,p,q,t;
+int checkError(int card[5]){
+  int i,t;
 
   //エラーチェック
   t=0;
@@ -14,20 +10,26 @@ int main(){
     //「1〜13」より範囲外の数字を除外
     if (card[i]<1 || card[i]>13 ){
       printf("\n1〜13の間で数字を入力して下さい\n\n");
-      return 0;
+      return 1;
     }
 
     //同じ数字のカードが５枚存在する場合を除外
-    if (card[i] == card[i+1]){
+    if (i<4 && card[i] == card[i+1]){
       t++;
       if (t==4){
           printf("\n同じ数字のカードは5枚存在しません！\n\n");
-          return 0;
+          return 1;
       }
     }
   }
+  return 0;
 
+}
 
+int score(int card[5]){
+
+  int tmp;
+  int p,q;
 
   //配列の中身を昇順にソート
   for (p=0; p<4; p++){
@@ -59,61 +61,119 @@ int main(){
       }
     }
   }
+  switch (r){
+    case 1:
+        break;
 
-  if (r==1){
-    printf("１ペアです！\n\n");
+    case 2:
+        break;
 
-  } else if (r==2){
-    printf("2ペアです！\n\n");
+    case 3:
+        break;
 
-  } else if (r==3){
-    printf("3カードです！\n\n");
+    case 4:
+        r = r+1;
+        break;
 
-  } else if (r==4){
-    printf("フルハウスです！\n\n");
+    case 6:
+        break;
 
-  } else if (r==6){
-    printf("4カードです！\n\n");
+    case 0:
+        //「1〜13」を循環するとき
+        if (card[4]==13 && card[0]==1){
+          int t=0;
+          for (p=0; p<4; p++){
+            for (q=p+1; q<5; q++){
 
-  } else if (r==0){
+              if (card[q]-card[p]==1){
+                t++;
+              }
+            }
+          }
 
-    //「1〜13」を循環するとき
-    if (card[4]==13 && card[0]==1){
-      int t=0;
-      for (p=0; p<4; p++){
-        for (q=p+1; q<5; q++){
+          if (t==3){
+              r = 4;
+              break;
+            //printf("ストレートです！\n\n");
 
-          if (card[q]-card[p]==1){
-            t++;
+          } else if (t!=3){
+              //printf("役がありません！\n\n");
+              r = 0;
+              break;
+          }
+
+        }
+
+        int u=0;
+        for (int i=0; i<4; i++){
+          if (card[i+1]-card[i]==1){
+            u++;
           }
         }
+        //「1〜13」の間の連番のとき
+        if (u==4){
+          //printf("ストレートです！\n\n");
+          r = 4;
+          break;
+
+        } else if (u!=4){
+          //printf("役がありません！\n");
+          r = 0;
+          break;
+        }
+
       }
-      if (t==3){
-        printf("ストレートです！\n\n");
 
-      } else if (t!=3){
-          printf("役がありません！\n\n");
-          return 0;
-      }
+  return r;
+}
 
-    }
+int main(){
+  //こちらにカードの手を入力して下さい
+  int card[5] = {11,10,11,10,10};
 
-    int s=0;
-    for (int i=0; i<4; i++){
-      if (card[i+1]-card[i]==1){
-        s++;
-      }
-    }
-    //「1〜13」の間の連番のとき
-    if (s==4){
-      printf("ストレートです！\n\n");
-
-    } else if (s!=4){
-      //printf("役がありません！\n");
-      return 0;
-    }
-
+  //エラーチェック
+  int error = checkError(card);
+  if (error==1){
+    return 0;
   }
 
+
+  int s = score(card);
+
+  switch (s){
+    case 1:
+        printf("１ペアです！\n\n");
+        break;
+
+    case 2:
+        printf("2ペアです！\n\n");
+        break;
+
+    case 3:
+        printf("3カードです！\n\n");
+        break;
+
+    case 4:
+        printf("ストレートです！\n\n");
+        break;
+
+    case 5:
+        printf("フルハウスです！\n\n");
+        break;
+
+    case 6:
+        printf("4カードです！\n\n");
+        break;
+
+    case 0:
+        printf("役がありません！\n\n");
+        break;
+
+    default:
+        return 0;
+        break;
+
+
+  }
 
 }
